aca14c720a894ca4c1c5da0d5d26fdc546ce3e57
alinaavv <160399232+alinaavv@users.noreply.github.com>
2024-06-07 16:28:42 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,15 +1,16 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
-#include <iostream>
+
+Train::Train() : first(nullptr), countOp(0) {}
 
 void Train::addCage(bool light) {
-    Cage *newCage = new Cage();
+    Cage* newCage = new Cage();
     newCage->light = light;
     if (!first) {
         first = newCage;
         first->next = first->prev = first;
     } else {
-        Cage *last = first->prev;
+        Cage* last = first->prev;
         last->next = newCage;
         newCage->prev = last;
         newCage->next = first;
@@ -21,7 +22,7 @@ int Train::getLength() {
     if (!first) return 0;
     first->light = true;
     int length = 1;
-    Cage *current = first->next;
+    Cage* current = first->next;
     while (!current->light) {
         current->light = true;
         current = current->next;
@@ -34,4 +35,8 @@ int Train::getLength() {
         current = current->next;
     }
     return length;
+}
+
+int Train::getOpCount() const {
+    return countOp;
 }
